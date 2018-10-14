fb85a0679ab9107487698216b0e53d7dd161b809
ShanXiY <ywg11_123@sina.com>
2018-10-14 22:33:23 +0800

    验证码设置

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,13 @@
 from info import redis_store
+from info.utils.captcha.captcha import captcha
 from . import index_blue
+from flask import render_template,current_app
 
-
+# 获取返回，一张图片
+@index_blue.route('/img_code')
+def img_code():
+    name,text,image_data = captcha.generate_captcha()
+    return image_data
 @index_blue.route('/')
 def hello_world():
 
@@ -24,4 +30,11 @@ def hello_world():
     # current_app.logger.warning("警告信息2")
     # current_app.logger.error("错误信息2")
 
-    return "helloworld1001"
+    return render_template("news/index.html")
+
+# 处理网站logo，浏览器在运行的时候，自动发送一个GET请求，向/favicon.ico地址
+# 只需要编写对应的接口，返回一张图片即可
+# 解决：current_app.send_static_file,自动向static文件夹中寻找指定的资源
+@index_blue.route('/favicon.ico')
+def web_logo():
+    return current_app.send_static_file("news/favicon.ico")
